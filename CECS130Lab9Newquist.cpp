3304431a5c433056f09b61e9c0aab9c307e69b52
canewq01 <canewq01@louisville.edu>
2017-10-27 13:36:38 -0400

    lab 8 completed

--- CECS130Lab9Newquist.cpp
+++ CECS130Lab9Newquist.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include "lab9.h"
+#include <cstdlib>
+#include <ctime>
 
-#define is ==
-#define isit if
+//Undefine user for AI v AI action
+#define USER 1
+
 
 /*
 Christopher Newquist
@@ -13,49 +16,141 @@ Lab 9: Tic Tac Toe
 */
 
 
-//NOTE: I defined a macro for ==. it is the word is.
 using namespace std;
 
 char Board::CheckForWin()
 {
-	isit((TL is TR && TC is TL ) or (TL is MC && MC is BR ))
-		return TL;
-	isit( (TR is MC && MC is BL )) //Top row and diagonals
-		return TR;
-	isit( (ML is MC && MC is MR ) ) //middle row
-		return MC;
-	isit( (BL is BC && BC is BR )) //bottom row
-	 	return BL;
-	isit (ML is TL && TL is BL ) //left column
-		return ML;
-	isit (MC is TC && TC is BC ) 
-		return TC;
-	isit (MR is TR && TR is BR ) 
-		return TR;
+	if( Array[0][0] == Array[0][1] && Array[0][2] == Array[0][0] ||  //Top row
+		Array[0][0] == Array[1][0] && Array[1][0] == Array[2][0] ||  //Right Column
+		Array[0][0] == Array[1][1] && Array[1][1] == Array[2][2]     //Left-to-Right Diagonal
+	)	
+		return Array[0][0];
+	if( Array[0][2] == Array[1][2] && Array[1][2] == Array[2][2] ||  //Left Column
+		Array[0][2] == Array[1][1] && Array[1][1] == Array[2][0]     //Right to Left Diagonal
+	)	
+		return Array[0][2];
+	if( Array[0][1] == Array[1][1] && Array[1][1] == Array[2][1] )  //Middle Column
+		return Array[0][1];
+	if(	Array[1][0] == Array[1][1] && Array[1][1] == Array[1][2] )//Middle Row
+		return Array[1][0];
+	if(	Array[2][0] == Array[2][1] && Array[2][1] == Array[2][2] )    //Bottom Row
+		return Array[2][0];
 	return NULL;
 		
 }
 
-
-Board::Board()
+int Board::PlayAI(char play, char other)
 {
-	TL= '1'; TC = '2'; TR = '3';
-	ML= '4'; MC = '5'; MR = '6';
-	BL= '7'; BC = '8'; BR = '9';
-	char Array[3][3]=
+	if(CheckForWin() == other || CheckForWin() == play){
+			//cout << "Game Over, " << CheckForWin() << " Wins!"<< endl;
+			return 11;
+		}
+	else
 	{
-		TL, TC, TR,
-		ML, MC, MR,
-		BL, BC, BR,	
-	};
+		
+		int open[9][2];
+		int nopen = 0;
+		for(int x=0; x<3; x++)
+			for(int y=0; y<3;y++)
+			{
+				if(Array[x][y] == ('1'+y+3*x))
+				{
+					//cout << "testing " << x << ','<< y<<endl;
+					Array[x][y] = play;
+					if(CheckForWin() == play)
+						return 3*x+y;
+					else
+					{
+						Array[x][y] = other;
+						if(CheckForWin() == other)
+						{
+							Array[x][y] = play;
+							return 3*x+y;
+						}
+						else
+						{
+							//cout << x << ','<< y<< " Is open" <<endl;
+							open[nopen][0]=x; open[nopen][1] = y;
+							nopen++;
+							Array[x][y] = 3*x+y + '1' ;
+						}
+					}
+				}
+			}
+		if(nopen > 0)
+		{
+			int i,x ,y;
+			i = rand()%nopen;
+			x = open[i][0], y = open[i][1];
+			Array[x][y] = play;
+			if(nopen == 1){
+				cout << "TIE" << endl;
+				tie = true;
+				return 10;
+			}
+		}
+	}	
 }
 
+void Board::PlayUser(char play, char opp)
+{
+	int loc, tries=0;
+	while(tries < 10)
+	{
+		cout << "Your Turn: Enter the number of the cell you wish to place your letter" << endl;
+		cin >> loc;
+		if(set(play, (loc-1)/3, (loc-1)%3))
+			break;
+		cout << "Bad Move...";
+	}
+	if(tries==9)
+	{
+		cout << "You couldn't make a move: moving for you..." << endl;
+		PlayAI(play, opp);
+	}
+}
+Board::Board()
+{
+	ResetBoard();
+	tie = false;
+}
+void Board::ResetBoard()
+{
+	Array[0][0]= '1'; Array[0][1] = '2'; Array[0][2] = '3';
+	Array[1][0]= '4'; Array[1][1] = '5'; Array[1][2] = '6';
+	Array[2][0]= '7'; Array[2][1] = '8'; Array[2][2] = '9';
+	
+}
+
+bool Board::isTie()
+{
+	bool loopBroken;
+	for(int x=0; x<3; x++)
+		for(int y =0; y<3; y++)
+		{
+			char c = 3*x+y+'1';
+			char a = Array[x][y];
+		//	cout << a << ' ' << c << endl;
+			if( c == a)
+			{
+				//cout << "The Loop is Broken ! "<<endl;
+				loopBroken = true;
+				goto end; //I tried a break, it wasn't working
+			}
+			loopBroken = false;
+		}
+		end:
+	return !loopBroken;
+}
 void Board::PrintBoard()
 {
-	cout << "____________"<< endl;
-	cout << TL << '|' << TC << '|' << TR << endl;
-	cout << ML << '|' << MC << '|' << MR << endl;
-	cout << BL << '|' << BC << '|' << BR << endl;
+	cout << " _____________"<< endl << endl;
+	cout << " | "<< Array[0][0] << " | " << Array[0][1] << " | " << Array[0][2] <<" |" << endl ;
+	cout << " _____________"<< endl << endl;
+	cout << " | "<< Array[1][0] << " | " << Array[1][1] << " | " << Array[1][2]  <<" |" << endl ;
+	cout << " _____________"<< endl << endl;
+	cout << " | "<< Array[2][0] << " | " << Array[2][1] << " | " << Array[2][2]  <<" |" <<endl;
+	cout << ' '<< "~~~~~~~~~~~~~"<< endl;
 }
 
 /* Sets Character based on location:
@@ -65,51 +160,69 @@ void Board::PrintBoard()
 
 */
 
-void Board::set(char p, int location)
+
+
+
+bool Board::set(char p, int x, int y)
 {
-	if(p is NULL)
-		cout << "Could not change, Try again" << endl;
-	else
-	switch(location)
-	{
-		case 1:
-			TL = p;
-			break;
-		case 2:
-			TC = p;
-			break;
-		case 3:
-			TR = p;
-			break;
-		case 4:
-			ML = p;
-			break;
-		case 5:
-			MC = p;
-			break;
-		case 6:
-			MR = p;
-			break;
-		case 7:
-			BL = p;
-			break;
-		case 8:
-			BC = p;
-			break;
-		case 9:
-			BR = p;
-			break;
-		default:
-			cout << "Bad Number, try again" <<endl;
+	char c = (3*x+y)+'1';
+	if ( p != NULL &&  Array[x][y] == c){
+	  Array[x][y] = p;
+	  return true;
+	}
+	else{
+	 		return false;
 	}
 }
 
 int main(){
 	Board game;
+	srand(time(0));
+	game.ResetBoard();
+	cout << "Instructions: "<<endl<<"Enter the number of the cell you wish to put X in. Board is as follows"<<endl;
 	game.PrintBoard();
-	for(int i=1; i<10; i++)
-		game.set('o', i);
-	game.PrintBoard();
-	cout << "Hello, World" << endl;
+	cout <<endl <<endl;
+	system("pause");
+	cout <<"Deciding who is going first... "<<endl;
+	bool first = rand()%2;
+	if(first)
+	  cout << "YOU are" << endl;
+	else
+		cout << "AI is going first" << endl;
+	int round = 0;
+	
+	do
+	{
+		if(first || round > 0)
+		{
+			cout << "Player: X" << endl;
+			#ifdef USER
+			
+				game.PrintBoard();
+				game.PlayUser('X', 'O');
+				
+			#endif
+			#ifndef USER
+			game.PlayAI('X', 'O');
+			#endif
+			game.PrintBoard();
+		}
+		if(game.isTie())
+			break;
+		cout << "Player: O" << endl;
+		game.PlayAI('O','X');
+		round++;
+		game.PrintBoard();
+	}while(game.CheckForWin()==NULL && !game.isTie());
+	char winner= game.CheckForWin();
+	if(game.isTie() && winner== NULL)
+		cout << "Game is tied" << endl;
+	else if( winner == 'X')
+	{
+		cout << "Game Over! You Win!" << endl;
+	}else{
+		cout << "Game Over! AI Wins! It will now take over your computer" << endl;
+	}
+	system("pause");
 	return 0;
 }
--- lab9.h
+++ lab9.h
@@ -2,14 +2,16 @@
 #define LAB9_H
 
 class Board {
-	char TL, TC, TR;
-	char ML, MC, MR;
-	char BL, BC, BR;
 	char Array[3][3];
+	bool tie;
 	public:
-		Board();       //set the board to empty
-		char CheckForWin(); //see if a win has occured on this board
-		void set(char,int);  //set position to char
-		void PrintBoard();  //print out the board
+		Board();       				// set the board to empty
+		char CheckForWin(); 		// see if a win has occured on this board
+		bool isTie();
+		bool set(char,int, int);  	// set position to char
+		void PrintBoard();	   		// print out the board
+		int PlayAI(char,char); 		// AI that will catch a win, prevent a loss, or place an arbitrary 
+		void PlayUser(char, char);			// User plays the game
+		void ResetBoard();			// Set the board to default configuration
 };
 #endif
